f9fa1e9d5983770f5d8dbbfa30674654ef69e9ea
Travis Poulsen <travis.poulsen@gmail.com>
2014-05-10 21:03:43 -0400

    Moved formatOutput to main; bound ingredient in convertUnits.

--- Backend/Recipe.hs
+++ Backend/Recipe.hs
@@ -1,9 +1,9 @@
 module Backend.Recipe
     (
       Ingredient(..)
+    , RecipeInfo
     , decomposeRecipe
     , handleLines
-    , formatOutput
     ) where
 ---------------------------------------
 --Imports
@@ -36,11 +36,6 @@ handleLines (n:c:i) = (n, conversion c, ingredients i)
   where ingredients = map (buildIngredient' (snd . conversion $ c) . words) 
         conversion  = calcConversion 
 
-formatOutput :: RecipeInfo -> String
-formatOutput (n,c,i) = "--Recipe\n" ++ n ++ "\n--Servings:\nScaled original by " ++ scaling ++ " to get " ++ servings ++" serving(s).\n--Ingredients\n" ++ ingredients
-  where ingredients = unlines . map show $ i
-        scaling     = show . snd $ c
-        servings    = show . fst $ c
 calcConversion :: String -> ConversionFactor
 calcConversion x = (wanted x, wanted x / given x    )
     where given  = read . head . words
@@ -53,11 +48,11 @@ buildIngredient' :: Float -> [String] -> Ingredient
 buildIngredient' c (v:u:i) = convertUnits $ Ingredient (read v * c) u (unwords i)
 
 convertUnits :: Ingredient -> Ingredient
-convertUnits (Ingredient a u i) 
-    | a < 1 = case map toLower u of
-                "kg" -> Ingredient (a * 1000) "g" i
-                "l"  -> Ingredient (a * 1000) "mL" i
-    | a > 1000 = case map toLower u of
+convertUnits ingredient@(Ingredient a u i) 
+    | a < 1     = case map toLower u of
+                  "kg" -> Ingredient (a * 1000) "g" i
+                  "l"  -> Ingredient (a * 1000) "mL" i
+    | a > 1000  = case map toLower u of
                   "g"  -> Ingredient (a / 1000) "kg" i
                   "ml" -> Ingredient (a / 1000) "L" i
-    | otherwise = Ingredient a u i
+    | otherwise = ingredient
--- RecipeScaler.hs
+++ RecipeScaler.hs
@@ -6,3 +6,10 @@ main = do
     recipe <- readFile inputFile
     writeFile outputFile $ formatOutput . handleLines . decomposeRecipe $ recipe
     putStrLn $ "Scaled your recipe and saved results as " ++ show outputFile ++ "."
+
+formatOutput :: RecipeInfo -> String
+formatOutput (n,c,i) = "--Recipe\n" ++ n ++ "\n--Servings:\nScaled original by " 
+                     ++ scaling ++ " to get " ++ servings ++" serving(s).\n--Ingredients\n" ++ ingredients
+  where ingredients = unlines . map show $ i
+        scaling     = show . snd $ c
+        servings    = show . fst $ c
